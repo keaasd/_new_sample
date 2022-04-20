974b690f7ad24680b046fc47ab5fae98b4c8af79
“Евгений <kea-asd@yandex.ru>
2022-04-21 02:34:08 +0700

    мобилка

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -1,6 +1,21 @@
-function openModal(burg) {
-    document.getElementById("modal").style.left = "0px";
+let burger = document.querySelector('.burger-btn__open');
+let menu = document.querySelector('.modal__mb');
+let burgerclose = document.querySelector('.burger-btn__close');
+const body = document.body;
+
+if (burger && menu) {
+    burger.onclick = () => {
+        menu.classList.toggle('burger_active')
+        body.classList.add("_locked");
+    }
 }
-function closeModal() {
-    document.getElementById("modal").style.left = "-200%";
-}
+burgerclose.addEventListener('click', () => {
+    menu.classList.remove('burger_active')
+    body.classList.remove("_locked");
+});
+
+menu.addEventListener('click', e => {
+    if (!e.target.classList.contains('modal__mb_window')) {
+        menu.classList.remove('burger_active')
+    }
+})
