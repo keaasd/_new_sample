04deee1e02a1a70bc8ecf359456b3ed370c78ef0
“Евгений <kea-asd@yandex.ru>
2022-05-22 01:31:42 +0700

    modal-close

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ let tabButton = document.querySelector('.category__button');
 let gridItem = document.querySelectorAll('.catalog-grid__item');
 // по клику на кнопку (tabButton) добавляем класс (active display: block) к табличкам (catalog-grid__item)
 
-tabButton.onclick = () => {
-  for (let i = 0; i < gridItem.length; i++) {
-    if (i < 8) {
-      gridItem[i].classList.add('catalog-grid_active')
-    }
+// tabButton.onclick = () => {
+//   for (let i = 0; i < gridItem.length; i++) {
+//     if (i < 8) {
+//       gridItem[i].classList.add('catalog-grid_active')
+//     }
     
-  }
+//   }
 
-}
+// }
