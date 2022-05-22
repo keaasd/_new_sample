84113de7afce66c7655f1e7080b1a765fdf95b2e
“Евгений <kea-asd@yandex.ru>
2022-05-22 22:35:58 +0700

    Выбор браузера

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 let tabButton = document.querySelector('.category__button');
 let gridItem = document.querySelectorAll('.catalog-grid__item');
-// по клику на кнопку (tabButton) добавляем класс (active display: block) к табличкам (catalog-grid__item)
 
 // tabButton.onclick = () => {
 //   for (let i = 0; i < gridItem.length; i++) {
@@ -11,3 +10,38 @@ let gridItem = document.querySelectorAll('.catalog-grid__item');
 //   }
 
 // }
+{
+var hasWebRTC = navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia;
+const body = document.body;
+if (hasWebRTC) {
+//   alert('This browser is fully or partially WebRTC-capable');
+  console.log('поддерживает WebRTC');
+} else {
+    console.log('Этот браузер НЕ поддерживает WebRTC');
+}
+var sBrowser, sUsrAg = navigator.userAgent;
+if (sUsrAg.indexOf("Firefox") > -1) {
+     sBrowser = "Mozilla Firefox";
+     body.classList.add('firefox');
+} else if (sUsrAg.indexOf("Opera") > -1) {
+     sBrowser = "Opera";
+     body.classList.add('Opera');
+} else if (sUsrAg.indexOf("Trident") > -1) {
+     sBrowser = "Microsoft Internet Explorer";
+     body.classList.add('ie');
+} else if (sUsrAg.indexOf("Edge") > -1) {
+     sBrowser = "Microsoft Edge";    
+     body.classList.add('Edge');   
+} else if (sUsrAg.indexOf("Chrome") > -1) {
+    sBrowser = "Google Chrome or Chromium";
+    body.classList.add('Chrome');    
+} else if (sUsrAg.indexOf("Safari") > -1) {
+    sBrowser = "Apple Safari";
+    body.classList.add('Safari'); 
+} else {
+    sBrowser = "unknown";
+}
+}
